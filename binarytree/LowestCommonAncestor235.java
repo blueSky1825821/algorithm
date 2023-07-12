bd87eb67b73e46306fa2ff0bb7af1377776e63c7
beacon <1664637427@qq.com>
2023-07-13 07:11:57 +0800

    feat: LowestCommonAncestor235

---
+++ binarytree/LowestCommonAncestor235.java
@@ -0,0 +1,31 @@
+package binarytree;
+
+public class LowestCommonAncestor235 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = null;
+        if (root.val > p.val || root.val > q.val) {
+            left = lowestCommonAncestor(root.left, p, q);
+        }
+        TreeNode right = null;
+        if (root.val < p.val || root.val < q.val) {
+            right = lowestCommonAncestor(root.right, p, q);
+        }
+
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left != null && right == null) {
+            return left;
+        } else if (left == null && right != null) {
+            return right;
+        } else {
+            return null;
+        }
+    }
+
+
+}
